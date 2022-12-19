9944098ba0434e466af48969921018c0bac37ee7
NguyenBaNhatHao <jdjjdjjdjdjdjddd@gmail.com>
2022-12-19 08:46:37 +0700

    success

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ var Database = Environment.GetEnvironmentVariable("Database");
 var UserId = Environment.GetEnvironmentVariable("UserId");
 var password = Environment.GetEnvironmentVariable("password");
 
-var connectionString="Data Source="+server+";Initial Catalog="+Database+";User Id="+UserId+";Password="+password+";";
+var connectionString="Data Source="+server+";Initial Catalog="+Database+";User Id="+UserId+";Password="+password+";Connection Timeout=30";
 var ApiKey = Environment.GetEnvironmentVariable("apiKey");
 var walletId = Environment.GetEnvironmentVariable("walletId");
 var transactionsID = String.Empty;
@@ -31,7 +31,8 @@ static async Task GetAccessTransaction(HttpClient client,string? walletId,string
     JObject jObject = JObject.Parse(responseString);
     var direction = jObject["data"]["items"];
     foreach(var item in direction){
-        if(item["direction"].Value<string>().Equals("outgoing") && item["fungibleTokens"].HasValues == true){
+        Console.WriteLine(item);
+        if(item["direction"].Value<string>().Equals("incoming") && item["fungibleTokens"].HasValues == true){
             uSDTTransactions.Address = item["fungibleTokens"][0]["recipient"].Value<string>();
             uSDTTransactions.Account = uSDTTransactions.Address;
             uSDTTransactions.Amount = item["fungibleTokens"][0]["amount"].Value<double>().ToString();
@@ -43,24 +44,48 @@ static async Task GetAccessTransaction(HttpClient client,string? walletId,string
             Console.WriteLine("Amount: "+uSDTTransactions.Amount);
             Console.WriteLine("TransactionHash: "+uSDTTransactions.TransactionHash);
             Console.WriteLine();
+
+            var _pConfirmations = 10;
+            var _pStatus = 0;
+            int _pResultMessageID = 1;
+            string _pResultMessageCode = "BTCTRANSACTIONS_EXISTED";
             var pAddress = new SqlParameter("@pAddress", uSDTTransactions.Address);
             var pAccount = new SqlParameter("@pAccount", uSDTTransactions.Account);
             var pTransactionHash = new SqlParameter("@pTransactionHash", uSDTTransactions.TransactionHash);
             var pAmount = new SqlParameter("@pAmount", uSDTTransactions.Amount);
+            var pConfirmations = new SqlParameter("@pConfirmations",_pConfirmations);
+            var pStatus = new SqlParameter("@pStatus",_pStatus);
+            var pResultMessageID = new SqlParameter("@pResultMessageID", _pResultMessageID);
+            var pResultMessageCode = new SqlParameter("@pResultMessageCode", _pResultMessageCode);
+
+            var pResultMessageID_exc = new SqlParameter("@pResultMessageID", _pResultMessageID);
+            var pResultMessageCode_exc = new SqlParameter("@pResultMessageCode", _pResultMessageCode);
 
             using (var conn = new SqlConnection(connectionString))
-            using (var command = new SqlCommand("usp_USDTTransactions_Insert", conn) { 
+            using (var command = new SqlCommand("[app].[usp_USDTTransactions_Insert]", conn) { 
                                     CommandType = System.Data.CommandType.StoredProcedure }) {
                                     command.Parameters.Add(pAddress);
                                     command.Parameters.Add(pAccount);
                                     command.Parameters.Add(pTransactionHash);
                                     command.Parameters.Add(pAmount);
+                                    command.Parameters.Add(pConfirmations);
+                                    command.Parameters.Add(pStatus);
+                                    command.Parameters.Add(pResultMessageID);
+                                    command.Parameters.Add(pResultMessageCode);
+            conn.Open();
+            command.ExecuteNonQuery();
+            conn.Close();
+            }
+            using (var conn = new SqlConnection(connectionString))
+            using (var command = new SqlCommand("[app].[usp_USDTTransactions_Execute]", conn) { 
+                                    CommandType = System.Data.CommandType.StoredProcedure }) {
+                                    command.Parameters.Add(pResultMessageID_exc);
+                                    command.Parameters.Add(pResultMessageCode_exc);
             conn.Open();
             command.ExecuteNonQuery();
             conn.Close();
             }
 
-            
         }
         
     }
